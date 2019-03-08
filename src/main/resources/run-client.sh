7ccecd40e2a3d8e1885249c82ce55dd60cfb322e
Koby Aharon <kobyahron@gmail.com>
2019-03-09 01:11:16 +0200

    Sign server and client certificates using an intermediate certificate

--- src/main/resources/run-client.sh
+++ src/main/resources/run-client.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-curl -E ${DIR}/keystore/cid.crt.pem:changeit \
+curl -E ${DIR}/keystore/cid.crt.pem:changeit -vvv \
     --key ${DIR}/keystore/cid.key.pem \
-    --cacert ${DIR}/keystore/ca.crt -vv \
+    --cacert ${DIR}/keystore/ca.crt \
     https://localhost:8443/greeting
